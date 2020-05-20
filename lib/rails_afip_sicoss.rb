3397155ff2b3210f69717f89e7193b808301c515
nicolasrsande <nicolasrsande@gmail.com>
2020-05-20 17:24:11 -0300

    bump version

--- lib/rails_afip_sicoss.rb
+++ lib/rails_afip_sicoss.rb
@@ -1,9 +1,9 @@
 require "fixy"
 require "rails_afip_sicoss/version"
-require "rails_afip_sicoss/documents/importacion_empleados"
-require "rails_afip_sicoss/records/empleado"
 require "rails_afip_sicoss/fixy/formatter/numeric"
 require "rails_afip_sicoss/fixy/formatter/numeric_float_l"
+require "rails_afip_sicoss/records/empleado"
+require "rails_afip_sicoss/documents/importacion_empleados"
 
 module RailsAfipSicoss
   class Error < StandardError; end
