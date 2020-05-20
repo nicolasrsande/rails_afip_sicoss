03f54fc2c30d59a4c4bada0f4259b88d71a6bb73
nicolasrsande <nicolasrsande@gmail.com>
2020-05-20 16:58:49 -0300

    Change formats to comply to sicoss

--- lib/rails_afip_sicoss.rb
+++ lib/rails_afip_sicoss.rb
@@ -3,7 +3,7 @@ require "rails_afip_sicoss/version"
 require "rails_afip_sicoss/documents/importacion_empleados"
 require "rails_afip_sicoss/records/empleado"
 require "rails_afip_sicoss/fixy/formatter/numeric"
-require "rails_afip_sicoss/fixy/formatter/numeric_currency"
+require "rails_afip_sicoss/fixy/formatter/numeric_float_l"
 
 module RailsAfipSicoss
   class Error < StandardError; end
--- lib/rails_afip_sicoss/fixy/formatter/numeric_float_l.rb
+++ lib/rails_afip_sicoss/fixy/formatter/numeric_float_l.rb
@@ -1,11 +1,11 @@
 module Fixy
   module Formatter
-    module NumericCurrency
-      def format_numeric_currency(input, length)
-        input = format('%.2f', input).to_s.delete('.')
+    module NumericFloatL
+      def format_numeric_float_l(input, length)
+        input = format('%.2f', input).to_s.gsub('.', ',')
         raise ArgumentError, "Invalid Input (only digits are accepted)" unless input =~ /^\d+$/
         raise ArgumentError, "Not enough length (input: #{input}, length: #{length})" if input.length > length
-        input.rjust(length, '0')
+        input.ljust(length, ' ')
       end
     end
   end
